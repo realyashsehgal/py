4f4416a30cdf9a4671a4eb2cab2a4a92ac85d99b
realyashsehgal <yash.sehgal1234@gmail.com>
2024-06-20 20:13:54 +0530

    list and tuple

---
+++ list.py
@@ -0,0 +1,21 @@
+# # main = {"pizza", "choweimen", "burger", "patty"}
+# # desert = {"choclate", "cake", "pastry"}
+# # food = [main,desert]
+# # print(food)
+# #okay lets make a matrix using lists more like 2d list
+# rows = int(input("Please enter the number of rows you want to add in you matrix"))
+# column = int(input("Please enter the number of columns you want to add in your matrix"))
+# matrix = [[0 for _ in range(column)] for _ in range(rows)]
+
+# for i in range(rows):
+#     for j in range(column):
+#         matrix[i][j] = int(input("Now please add the input for position " +str(i)+str(j)))
+# print(matrix)
+
+
+#okay so lemme show you some example of tuple
+students = [ "Yash",19,"one piece","luffy",19]
+print(students)
+#This is sets
+student = { "Yash Sehgal", 19, "ore no nawa eren yeager",19}
+print(student)
