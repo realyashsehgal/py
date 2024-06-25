08547af8017f3c606ca86c37a2096282a94aeb4a
realyashsehgal <yash.sehgal1234@gmail.com>
2024-06-26 01:21:11 +0530

    awlrus operator

---
+++ walrusop.py
@@ -0,0 +1,6 @@
+#for example let there be a variable happy
+happy = True
+print(happy)
+#but what if we can declare happy as true in print statement huh 
+# print(happy = True) #Like this but it gives error so
+print(happy:=True)#Thats a walrus operator it works like this
