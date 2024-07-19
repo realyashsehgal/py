60bf8883de1e03d15ad2efe5a80349b1326d2a7b
realyashsehgal <yash.sehgal1234@gmail.com>
2024-07-19 19:09:21 +0530

    tic tac toe done

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,6 +1,13 @@
 from tkinter import *
 #WORKING
+def turncount():
+    global turnss
+    if(turn%2==0):
+        turnss.config(text="Player X turn")
+    else:
+        turnss.config(text="Player O turn")
 def placed(row,col,box):
+
     global turn
     global boardrn
     if boardrn[row][col] == "*":
@@ -12,22 +19,47 @@ def placed(row,col,box):
                 boardrn[row][col] = "O"
             if checkWind(boardrn):
                 if turn % 2 == 0:
+                    WindWindow = Tk()
                     print("Player X wins!")
+                    Winner = Label(WindWindow,text="Player X wins the game",font=('arial',50,'bold')).pack()
+                    WindWindow.mainloop()
                 else:
+                    WindWindow = Tk()
                     print("Player O wins!")
-            elif turn == 8:  # Check for draw
+                    Winner = Label(WindWindow,text="Player O wins the game",font=('arial',50,'bold')).pack()
+                    WindWindow.mainloop()
+            elif turn == 9:  # Check for draw
+                WindWindow = Tk()
                 print("It's a draw!")
+                Winner = Label(WindWindow,text="Its a draw",font=('arial',50,'bold')).pack()
+                WindWindow.mainloop()
             turn += 1
     else:
             print("It's taken")
+    turncount()
 
+
+#RESTART
+def restart():
+     global boardrn
+     global turn
+     boardrn = [["*","*","*"],
+                ["*","*","*"],
+                ["*","*","*"]]
+     
+     for row in range(3):
+        for col in range(3):
+            box = Button(board, font=('arial', 20), height=2, width=5)
+            box.config(command=lambda r=row, c=col, b=box: placed(r, c, b))
+            box.grid(row=row, column=col)
+     turn = 1
 
 
 
 
 #CHECKING WINNING
 def checkWind(boardrn):
-    #diagnols
+    
     for i in range(3):
         if boardrn[i][0] == boardrn[i][1] == boardrn[i][2] != "*":
             return True
@@ -46,6 +78,8 @@ def checkWind(boardrn):
 
 
 
+# X = threading.Thread(target=turncount,args=())
+# X.start()
 turn = 1
 boardrn = [["*","*","*"],
            ["*","*","*"],
@@ -58,6 +92,9 @@ Window.geometry("600x600")
 Heading = Label(Window,text="Tic-Tac-Toe",font=('arial',20,'bold'))
 Heading.pack()
 
+#TURN THING
+turnss = Label(Window,text="Player O turn",font=('arial',25))
+turnss.pack()
 #BOARD
 board = Frame(Window)
 board.pack()
@@ -72,7 +109,7 @@ gap = Label(Window,height=3).pack()
 
 
 #RESTART
-restart = Button(Window,text="Restart",font=('arial',20,'bold'))
+restart = Button(Window,text="Restart",font=('arial',20,'bold'),command=restart)
 restart.pack()
-
+# X.join()
 Window.mainloop()
